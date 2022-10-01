5b4ef27487f7377cc3b2d4954c20045d3df6303a
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-01 14:30:52 +0200

    use jdbc-user-details-manager for user management

--- src/main/java/com/bsib/config/ProjectConfig.java
+++ src/main/java/com/bsib/config/ProjectConfig.java
@@ -1,23 +1,19 @@
 package com.bsib.config;
 
-import com.bsib.model.User;
-import com.bsib.services.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("blessed", "1234pass", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into `ssia_ch3`.`authorities` values (null, 'blessed', 'write');
+insert into `ssia_ch3`.`users` values (null, 'blessed', '1234pass', '1');

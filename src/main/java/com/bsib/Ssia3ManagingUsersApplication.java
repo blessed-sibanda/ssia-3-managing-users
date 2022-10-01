a5d9f27431108c765022c1b885a295b760afeb95
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-10-01 13:10:50 +0200

    implement in-memory-user-details service

---
+++ src/main/java/com/bsib/Ssia3ManagingUsersApplication.java
@@ -0,0 +1,13 @@
+package com.bsib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssia3ManagingUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ssia3ManagingUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bsib/Ssia3ManagingUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssia3ManagingUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
